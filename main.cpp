3f17336cea1cd423154d3de9f1b9d2056d8b3453
Jalen Jones <jalcjon1@ut.utm.edu>
2025-01-25 23:21:31 -0600

    second main commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
-
 #include <iostream>
 
 using namespace std;
 
+  int sum(int n){
+      int total = 0;
+    for(int i = 1; i <= n; i++){
+        total+=i;
+    }
+    return total;
+  }
+
+  int prod(int n){
+      int total = 1;
+    for(int i = 1; i <= n; i++){
+        total*=i;
+    }
+    return total;
+  }
+
 int main() {
+  int n;
   
+  cout << "Enter a number: ";
+  cin >> n;
   
-  return 0;
-}
+  cout << "The sum of the numbers up to n are: " << sum(n) << "\n";
+  cout << "The pproduct of the numbers up to n are: " << prod(n);
+  
+ }
